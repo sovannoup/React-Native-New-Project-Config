60aaeacf647d79122bb076de8b612a8c5f1ca1e5
noupsovan <noupsovan18@kit.edu.kh>
2022-05-14 16:44:46 +0700

    Initial Commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import {
+  View,
+} from 'react-native'
+import { Provider } from 'react-redux';
+import store from './src/Store';
+import Navigation from './src/Navigation/index';
+import NavigationService from './src/Service/navigationService';
+export default class App extends Component {
+
+  render() {
+    return (
+      <Provider store={store}>
+        <View style={{ flex: 1 }}>
+          <Navigation
+            ref={navigatorRef =>
+              NavigationService.setTopLevelNavigator(navigatorRef)
+            }
+          />
+        </View>
+      </Provider>
+    );
+  }
+}
