c6668165cb446b0973c7b6e35200472dfe7ae891
noupsovan <noupsovan18@kit.edu.kh>
2022-05-21 18:49:45 +0700

    add nav buttom

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -7,101 +7,157 @@ import {
   Platform,
   TouchableOpacity,
 } from 'react-native';
-import {createAppContainer} from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack'
-import {createBottomTabNavigator} from 'react-navigation-tabs'
-import {NAV_TYPES} from './navTypes'
-import screenLogin from '../Containers/LoginContainer'
-import screenHome from '../Containers/HomeContainer'
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {NAV_TYPES} from './navTypes';
+import screenLogin from '../Containers/LoginContainer';
+import screenHome from '../Containers/HomeContainer';
 
 const IntroNavigator = createStackNavigator(
-    {
-        [NAV_TYPES.LOGIN]:{
-            screen:screenLogin,
-            navigationOptions:{
-                headerShown:false,
+  {
+    [NAV_TYPES.LOGIN]: {
+      screen: screenLogin,
+      navigationOptions: {
+        headerShown: false,
       },
-        },
+    },
   },
-    {
-        initialRouteName:NAV_TYPES.LOGIN,
-    }
-    )
+  {
+    initialRouteName: NAV_TYPES.LOGIN,
+  },
+);
 
-
-const HomeNavigation = createStackNavigator(
-    {
-        [NAV_TYPES.MAIN_HOME]:{
-            screen:screenHome,
-            navigationOptions:{
-                headerShown:false
-            }
-        },
-    }
-)
-
+const ExploreNavigation = createStackNavigator({
+  [NAV_TYPES.MAIN_EXPLORE]: {
+    screen: screenHome,
+    navigationOptions: {
+      headerShown: false,
+    },
+  },
+});
+const LedgerNavigation = createStackNavigator({
+  [NAV_TYPES.MAIN_LEDGER]: {
+    screen: screenHome,
+    navigationOptions: {
+      headerShown: false,
+    },
+  },
+});
+const FavNavigation = createStackNavigator({
+  [NAV_TYPES.MAIN_FAV]: {
+    screen: screenHome,
+    navigationOptions: {
+      headerShown: false,
+    },
+  },
+});
+const ProfileNavigation = createStackNavigator({
+  [NAV_TYPES.MAIN_PROFILE]: {
+    screen: screenHome,
+    navigationOptions: {
+      headerShown: false,
+    },
+  },
+});
 
 //Tab Navigation
 const TabNavigation = createBottomTabNavigator(
-    {
-        [NAV_TYPES.HOME]:{
-            screen:HomeNavigation,
-            navigationOptions:{
-                tabBarLabel:({focused})=>(
-                    <Text style={{textAlign:'center',color:focused ? '#c43e00':'black'}}>ទំព័រដើម</Text>
-                ),
-            }
-        },
+  {
+    [NAV_TYPES.EXPLORE]: {
+      screen: ExploreNavigation,
+      navigationOptions: {
+        tabBarLabel: ({focused}) => (
+          <Text
+            style={{textAlign: 'center', color: focused ? '#c43e00' : 'black'}}>
+            ទំព័រដើម
+          </Text>
+        ),
+      },
     },
-    {
-        tabBarOptions: {
-          showIcon: true,
-          showLabel:true,
-        //   activeBackgroundColor:'#f5f5f5',
-        
-          labelStyle: {
-            fontSize: 20,
-          },
-          style:{height:Platform.OS === 'ios' ? 60:63,paddingBottom:Platform.OS === 'ios' ? 0 : 10},
-          activeColor:'blue',
-        activeTabStyle:{backgroundColor:'blue'},
-        
-        },
-    }
+    [NAV_TYPES.EXPLORE]: {
+      screen: LedgerNavigation,
+      navigationOptions: {
+        tabBarLabel: ({focused}) => (
+          <Text
+            style={{textAlign: 'center', color: focused ? '#c43e00' : 'black'}}>
+            ទំព័រដើម
+          </Text>
+        ),
+      },
+    },
+    [NAV_TYPES.EXPLORE]: {
+      screen: FavNavigation,
+      navigationOptions: {
+        tabBarLabel: ({focused}) => (
+          <Text
+            style={{textAlign: 'center', color: focused ? '#c43e00' : 'black'}}>
+            ទំព័រដើម
+          </Text>
+        ),
+      },
+    },
+    [NAV_TYPES.EXPLORE]: {
+      screen: ProfileNavigation,
+      navigationOptions: {
+        tabBarLabel: ({focused}) => (
+          <Text
+            style={{textAlign: 'center', color: focused ? '#c43e00' : 'black'}}>
+            ទំព័រដើម
+          </Text>
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      showIcon: true,
+      showLabel: true,
+      //   activeBackgroundColor:'#f5f5f5',
 
-)
-
+      labelStyle: {
+        fontSize: 20,
+      },
+      style: {
+        height: Platform.OS === 'ios' ? 60 : 63,
+        paddingBottom: Platform.OS === 'ios' ? 0 : 10,
+      },
+      activeColor: 'blue',
+      activeTabStyle: {backgroundColor: 'blue'},
+    },
+  },
+);
 
 const CoreNavigation = createStackNavigator(
-    {
-        [NAV_TYPES.MAIN]:{
-            screen:TabNavigation,
-            navigationOptions: {
-                headerShown:false
-              }
-        },
+  {
+    [NAV_TYPES.MAIN]: {
+      screen: TabNavigation,
+      navigationOptions: {
+        headerShown: false,
+      },
     },
-    {
-        initialRouteName:NAV_TYPES.MAIN
-    }
-)
+  },
+  {
+    initialRouteName: NAV_TYPES.MAIN,
+  },
+);
 const MainNavigation = createStackNavigator(
-    {
-        [NAV_TYPES.INTRO]:{
-            screen:IntroNavigator,
-            navigationOptions:{
-                headerShown:false
-            }
-        },
-        [NAV_TYPES.CORE]:{
-            screen:CoreNavigation,
-            navigationOptions:{
-                headerShown:false
-            }
-        }
+  {
+    [NAV_TYPES.INTRO]: {
+      screen: IntroNavigator,
+      navigationOptions: {
+        headerShown: false,
+      },
     },
-    {
-        initialRouteName:NAV_TYPES.INTRO
-    }
-)
-export default createAppContainer(MainNavigation)
+    [NAV_TYPES.CORE]: {
+      screen: CoreNavigation,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+  },
+  {
+    initialRouteName: NAV_TYPES.INTRO,
+  },
+);
+export default createAppContainer(MainNavigation);
--- src/Screens/ScreenHome.js
+++ src/Screens/ScreenHome.js
@@ -1,33 +1,21 @@
-import React,{Component} from 'react'
-import {
-    Text, StyleSheet, TouchableOpacity, View
-} from 'react-native'
-import NavigationService from '../Service/navigationService'
-import { NAV_TYPES } from '../Navigation/navTypes'
-export default class Home extends Component{
-    constructor(prop){
-        super(prop)
-        this.state={
-           
-        }
-        
-    }
-   
+import React, {Component} from 'react';
+import {Text, StyleSheet, TouchableOpacity, View} from 'react-native';
+import NavigationService from '../Service/navigationService';
+import {NAV_TYPES} from '../Navigation/navTypes';
+export default class Home extends Component {
+  constructor(prop) {
+    super(prop);
+    this.state = {};
+  }
 
-   componentDidMount(){
-       
-   }
-   UNSAFE_componentWillReceiveProps(nextProps){
-    
-   }
-    render(){
-        return(
-            <View style={{height: '100%', justifyContent: 'center'}}>
-                <Text style={{textAlign:'center'}}>Login</Text>
-            </View>
-        )
-    }
+  componentDidMount() {}
+  UNSAFE_componentWillReceiveProps(nextProps) {}
+  render() {
+    return (
+      <View style={{height: '100%', justifyContent: 'center'}}>
+        <Text style={{textAlign: 'center'}}>Login</Text>
+      </View>
+    );
+  }
 }
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({});
